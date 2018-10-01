bef367638b4005f978ea0e087cfe8cccd98d15db
Amistad Alex (ERNI) <amal@erni.ph>
2018-10-01 15:46:54 +0800

    update

--- core/api.js
+++ core/api.js
@@ -4,12 +4,11 @@ const request = require('request');
 module.exports = {
   get: function(url, callback) {
     request.get(`${settings.api_url}/wp-json/wc/v2/${url}`, {
-      oauth:{
-        consumer_key: settings.consumer_key,
-        consumer_secret: settings.consumer_secret,
-      },
-      query_string_auth: true,
-      qs:{user_id:'...'}
-    }, callback)
+      'auth': {
+        'user': 'ck_901a2d8d8f110651e91321d70bb0ba9c219997d0',
+        'pass': 'cs_284dbd5f36bf1264c96fd8e0e11bee2ad0702aeb',
+        'sendImmediately': false
+      }
+    })
   }
 }
