a45b3721ffb7626804cd3b358f49a1b0e47371ea
Amistad Alex (ERNI) <amal@erni.ph>
2018-10-01 15:14:31 +0800

    update http to https

--- settings/core.js
+++ settings/core.js
@@ -1,5 +1,5 @@
 module.exports = {
   consumer_key:'ck_4c2bf95ca8cdb4678aefc27e82a35f5416d1fbbe',
   consumer_secret:'cs_dc2fcf4181c23f6b5aceffa0046a01054c37f9ef',
-  api_url: 'http://thehypedistrict.com',
+  api_url: 'https://thehypedistrict.com',
 };
