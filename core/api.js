5cc6abd7162f39622d2e9557bb8ebc20f6cbbff4
Amistad Alex (ERNI) <amal@erni.ph>
2018-10-02 16:36:16 +0800

    update

--- core/api.js
+++ core/api.js
@@ -4,9 +4,10 @@ const request = require('request');
 module.exports = {
   get: function(url, callback) {
     request.get(`${settings.api_url}/wp-json/wc/v2/${url}`, {
-      oauth:{
-        consumer_key: settings.consumer_key,
-        consumer_secret: settings.consumer_secret,
+      auth:{
+        user: settings.consumer_key,
+        pass: settings.consumer_secret,
+        sendImmediately: false
       },
       qs:{user_id:'...'}
     }, callback)
